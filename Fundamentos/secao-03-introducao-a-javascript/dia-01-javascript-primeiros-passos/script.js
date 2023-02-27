b6970fa14bb7a5503d79e4d8af3ef3fb7861de33
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-02-27 11:09:27 -0300

    código com switch/case e .toLowerCase()

--- Fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
+++ Fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -1,11 +1,24 @@
-let ang1 = 45;
-let ang2 = 45;
-let ang3 = 90;
+let chess = "Rook".toLowerCase();
 
-if (ang1 + ang2 + ang3 == 180 && ang1 > 0 && ang2 > 0 && ang3 > 0) {
-  console.log("true");
-} else if (ang1 < 0 || ang2 < 0 || ang3 < 0) {
-  console.log("Algum ângulo é inválido. Reveja!");
-} else {
-  console.log("false");
+switch (chess) {
+  case "pawn":
+    console.log("move forward");
+    break;
+  case "bishop":
+    console.log("move diagonally");
+    break;
+  case "knight":
+    console.log("move horizontally and vertically");
+    break;
+  case "rook":
+    console.log("as many squares as it likes horizontally and vertically");
+    break;
+  case "queen":
+    console.log("as many squares as it likes horizontally and vertically");
+    break;
+  case "king":
+    console.log("one square in any direction");
+    break;
+  default:
+    console.log("Error. Invalid piece.");
 }
