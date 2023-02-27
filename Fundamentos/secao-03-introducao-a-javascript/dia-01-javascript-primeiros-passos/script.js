fecc416e88ac9daee334bf70939e65fbb2511ff6
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-02-27 10:18:55 -0300

    2.comparação com if/else, usando operadores lógicos

--- Fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
+++ Fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -1,13 +1,11 @@
-let numero1 = 20;
-let numero2 = 0;
-let numero3 = 12;
+let ang1 = 45;
+let ang2 = 45;
+let ang3 = 90;
 
-if (numero1 > numero2 && numero1 > numero3) {
-  console.log(numero1, "é o maior número");
-} else if (numero2 > numero1 && numero2 > numero3) {
-  console.log(numero2, "é o maior número");
-} else if (numero3 > numero1 && numero3 > numero2) {
-  console.log(numero3, "é o maior número");
+if (ang1 + ang2 + ang3 == 180 && ang1 > 0 && ang2 > 0 && ang3 > 0) {
+  console.log("true");
+} else if (ang1 < 0 || ang2 < 0 || ang3 < 0) {
+  console.log("Algum ângulo é inválido. Reveja!");
 } else {
-  console.log("Os números são iguais");
+  console.log("false");
 }
