12ae43504a1d669ed7b84e9b1181a0d67bfbfc28
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-02-27 15:56:23 -0300

    calculando valor de salário líquido com if/else e operadores lógicos/aritméticos

--- Fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
+++ Fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -1,13 +1,28 @@
-let valorCusto = -1;
-let valorVenda = 0;
-let impostoSobreCusto = (20 * valorCusto) / 100;
-let valorCustoTotal;
-let lucro;
+let aliquotaINSS;
+let aliquotaIR;
+let salarioBruto = 1556.93;
 
-if (valorCusto >= 0 && valorVenda >= 0) {
-  valorCustoTotal = valorCusto + impostoSobreCusto;
-  lucro = (valorVenda - valorCustoTotal) * 1000;
-  console.log("o lucro total da empresa ao vender mil produtos é", lucro);
+if (salarioBruto <= 1556.94) {
+  aliquotaINSS = salarioBruto * 0.08;
+} else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+  aliquotaINSS = salarioBruto * 0.09;
+} else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+  aliquotaINSS = salarioBruto * 0.11;
 } else {
-  console.log("Erro. Valores incorretos.");
+  aliquotaINSS = 570.88;
 }
+
+let salarioBase = salarioBruto - aliquotaINSS;
+
+if (salarioBase <= 1903.98) {
+  aliquotaIR = 0;
+} else if (salarioBase >= 1903.99 && salarioBase <= 2826.65) {
+  aliquotaIR = salarioBase * 0.075 - 142.8;
+} else if (salarioBase >= 2826.66 && salarioBase <= 3751.05) {
+  aliquotaIR = salarioBase * 0.15 - 354.8;
+} else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+  aliquotaIR = salarioBase * 0.225 - 636.13;
+} else if (salarioBase > 4664.68) {
+  aliquotaIR = salarioBase * 0.275 - 869.36;
+}
+console.log("Salário: ", salarioBase - aliquotaIR);
