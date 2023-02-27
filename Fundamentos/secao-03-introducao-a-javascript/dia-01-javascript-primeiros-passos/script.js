dba2f399fdd49808d8007c4ba8cd416e26c4461d
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-02-27 12:04:30 -0300

    2.comparação com if/else utilizando operadores lógicos/aritméticos

--- Fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
+++ Fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -1,8 +1,8 @@
 let number1 = 4;
-let number2 = 9;
+let number2 = 0;
 let number3 = 1;
 
-if (number1 % 2 == 0 || number2 % 2 == 0 || number3 % 2 == 0) {
+if (number1 % 2 != 0 || number2 % 2 != 0 || number3 % 2 != 0) {
   console.log("true");
 } else {
   console.log("false");
