c64710ec35f61693de462523fd8c95ef9c77f215
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-02-28 20:51:01 -0300

    ordenação de array em ordem crescente com bubble sort

--- Fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
+++ Fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
@@ -1,19 +1,13 @@
-let array = ["java", "javascript", "python", "html", "css"];
-let smallest = array[0];
-let biggest = array[0];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let index = 0; index < array.length; index += 1) {
-  if (array[index].length < smallest.length) {
-    smallest = array[index];
-  } else if (array[index].length > biggest.length) {
-    biggest = array[index];
+for (let index = 1; index < numbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (numbers[index] < numbers[secondIndex]) {
+      let position = numbers[index];
+      numbers[index] = numbers[secondIndex];
+      numbers[secondIndex] = position;
+    }
   }
 }
 
-// for (let index = 0; index < array.length; index += 1) {
-//   if (array[index].length > biggest.length) {
-//     biggest = array[index];
-//   }
-// }
-console.log("A menor palavra do array é: ", smallest);
-console.log("A maior palavra do array é: ", biggest);
+console.log(numbers);
