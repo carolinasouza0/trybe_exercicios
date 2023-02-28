b242c7de55206b7e08127f671171f1d784ade5fe
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-02-28 20:07:32 -0300

    mostrando a maior e menor palavra dentro de um array com o for

--- Fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
+++ Fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
@@ -1,5 +1,19 @@
-let word = "carolina";
+let array = ["java", "javascript", "python", "html", "css"];
+let smallest = array[0];
+let biggest = array[0];
 
-for (let index = word.length - 1; index >= 0; index -= 1) {
-  console.log(word[index]);
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length < smallest.length) {
+    smallest = array[index];
+  } else if (array[index].length > biggest.length) {
+    biggest = array[index];
+  }
 }
+
+// for (let index = 0; index < array.length; index += 1) {
+//   if (array[index].length > biggest.length) {
+//     biggest = array[index];
+//   }
+// }
+console.log("A menor palavra do array é: ", smallest);
+console.log("A maior palavra do array é: ", biggest);
