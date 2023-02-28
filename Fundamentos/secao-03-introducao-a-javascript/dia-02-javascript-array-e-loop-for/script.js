6a3329aa11907e2ddea4645e0f25bd121bc19bdd
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-02-28 18:21:10 -0300

    encontrando número ímpar no array com o for

--- Fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
+++ Fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
@@ -1,10 +1,13 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0;
-let maiorNumero = numbers[0];
+let numbers = [2, 4, 6, 8, 70, 8, 100, 2, 30, 26];
+let numerosImpares = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
-  if (numbers[index] > maiorNumero) {
-    maiorNumero = numbers[index];
+  if (numbers[index] % 2 !== 0) {
+    numerosImpares += 1;
   }
 }
-console.log(maiorNumero);
+if (numerosImpares === 0) {
+  console.log("Nenhum valor ímpar encontrado");
+} else {
+  console.log(numerosImpares);
+}
