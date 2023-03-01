19b4d4981f07948962ba7d9051d6e400208c99df
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-03-01 14:00:30 -0300

    criando novo array com multiplicação de numeros do antigo array utilizando for

--- Fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
+++ Fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/script.js
@@ -1,13 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let newNumbers = [];
 
-for (let index = 1; index < numbers.length; index += 1) {
-  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-    if (numbers[index] > numbers[secondIndex]) {
-      let position = numbers[index];
-      numbers[index] = numbers[secondIndex];
-      numbers[secondIndex] = position;
-    }
+for (let index = 0; index < numbers.length; index += 1) {
+  if (index < numbers.length - 1) {
+    newNumbers.push(numbers[index] * numbers[index + 1]);
+  } else {
+    newNumbers.push(numbers[index] * 2);
   }
 }
-
 console.log(numbers);
+console.log(newNumbers);
