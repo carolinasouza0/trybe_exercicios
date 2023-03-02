db6570f1f488a859f94d3d41b9a48519d9dee1c7
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-03-02 15:23:03 -0300

    entendendo método object.keys

--- Fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/script.js
+++ Fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/script.js
@@ -1,15 +1,29 @@
-let customer = {
-  firstName: "Roberto",
-  age: 22,
-  job: "Teacher",
+let student1 = {
+  html: "Muito Bom",
+  css: "Bom",
+  javascript: "Ótimo",
+  softskills: "Ótimo",
 };
 
-let newKey = "lastName";
-let lastName = "Ferreira";
+let student2 = {
+  html: "Bom",
+  css: "Ótimo",
+  javascript: "Ruim",
+  softskills: "Ótimo",
+  git: "Bom", // chave adicionada
+};
 
-function addProperty(object, key, value) {
-  object[key] = value;
+function listSkills(student) {
+  let arrayOfSkills = Object.keys(student);
+  for (let index in arrayOfSkills) {
+    console.log(
+      arrayOfSkills[index] + ", Nível: " + student[arrayOfSkills[index]]
+    );
+  }
 }
 
-addProperty(customer, newKey, lastName);
-console.log(customer);
+console.log("Estudante 1");
+listSkills(student1);
+
+console.log("Estudante 2");
+listSkills(student2);
