8ecadb05a0b49d1c3d2d5229d0eafec82d857136
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-03-02 11:38:41 -0300

    aprendendo sobre objetos (chaves e valor)

---
+++ Fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/script.js
@@ -0,0 +1,20 @@
+let player = {
+  firstName: "Marta",
+  lastName: "Silva",
+  age: 34,
+  medals: {
+    golden: 2,
+    silver: 3,
+  },
+};
+
+let meuObjeto = player;
+console.log(
+  "A jogadora " +
+    meuObjeto.firstName +
+    " " +
+    meuObjeto.lastName +
+    " tem " +
+    meuObjeto.age +
+    " anos de idade."
+);
