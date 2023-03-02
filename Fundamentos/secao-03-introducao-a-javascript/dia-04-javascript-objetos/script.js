42631e82aeff40d7b82ace731d94acab71dc86d0
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-03-02 11:51:23 -0300

    2. aprendendo sobre objetos (chaves e valor)

--- Fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/script.js
+++ Fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/script.js
@@ -8,13 +8,32 @@ let player = {
   },
 };
 
-let meuObjeto = player;
+player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+
 console.log(
   "A jogadora " +
-    meuObjeto.firstName +
+    player.firstName +
     " " +
-    meuObjeto.lastName +
+    player.lastName +
     " tem " +
-    meuObjeto.age +
+    player.age +
     " anos de idade."
+);
+
+console.log(
+  "A jogadora " +
+    player["firstName"] +
+    " " +
+    player["lastName"] +
+    " foi eleita a melhor do mundo por " +
+    player["bestInTheWorld"].length +
+    " vezes."
+);
+
+console.log(
+  "A jogadora possui " +
+    player.medals.golden +
+    " medalhas de outo e " +
+    player.medals.silver +
+    " medalhas de prata."
 );
