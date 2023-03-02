5c1ac40761a06d31dfc4c41d1fe682dfc0a49fee
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-03-02 18:33:56 -0300

    entendendo o método object.assign - objeto destino sendo vazio

--- Fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/script.js
+++ Fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/script.js
@@ -6,20 +6,7 @@ let lastName = {
   lastName: "Silva",
 };
 
-let clone = Object.assign(person, lastName);
-
-console.log(clone); // { name: 'Roberto', lastName: 'Silva' }
-console.log(person); // { name: 'Roberto', lastName: 'Silva' }
-
-clone.name = "Maria";
-
-console.log("Mudando a propriedade name através do objeto clone");
-console.log(clone); // Output: { name: 'Maria', lastName: 'Silva' }
-console.log(person); // Output: { name: 'Maria', lastName: 'Silva' }
-console.log("--------------");
-
-person.lastName = "Ferreira";
-
-console.log("Mudando a propriedade lastName através do objeto person");
-console.log(clone); // Output: { name: 'Maria', lastName: 'Ferreira' }
-console.log(person); // Output: { name: 'Maria', lastName: 'Ferreira' }
+let newPerson = Object.assign({}, person, lastName);
+newPerson.name = "Gilberto";
+console.log(newPerson);
+console.log(person);
