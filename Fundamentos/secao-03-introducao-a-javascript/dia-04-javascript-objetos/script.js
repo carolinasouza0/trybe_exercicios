9fd30fbda66f260a8fd656f4ca80df1be1086e47
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-03-03 09:56:02 -0300

    resolvendo exercicios sobre objetos

--- Fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/script.js
+++ Fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/script.js
@@ -1,12 +1,30 @@
-let person = {
-  name: "Roberto",
+let leitor = {
+  nome: "Julia",
+  sobrenome: "Pessoa",
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: "O Pior Dia de Todos",
+      autor: "Daniela Kopsch",
+      editora: "Tordesilhas",
+    },
+  ],
 };
 
-let lastName = {
-  lastName: "Silva",
-};
+console.log(
+  "O livro favorito de " +
+    leitor.nome +
+    " " +
+    leitor.sobrenome +
+    ' se chama "' +
+    leitor.livrosFavoritos[0].titulo +
+    '"'
+);
 
-let newPerson = Object.assign({}, person, lastName);
-newPerson.name = "Gilberto";
-console.log(newPerson);
-console.log(person);
+leitor.livrosFavoritos.push({
+  titulo: "Harry Potter e o Prisioneiro de Azkaban",
+  autor: "JK Rowling",
+  editora: "Rocco",
+});
+
+console.log("Julia tem " + leitor.livrosFavoritos.length + " livros favoritos");
