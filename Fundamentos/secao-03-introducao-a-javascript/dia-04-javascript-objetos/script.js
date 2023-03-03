cbcfd9908e37a28a54a1944dc005ee0a63e3efcb
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-03-03 10:36:20 -0300

    manipulando objetos com funções

--- Fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/script.js
+++ Fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/script.js
@@ -1,30 +1,83 @@
-let leitor = {
-  nome: "Julia",
-  sobrenome: "Pessoa",
-  idade: 21,
-  livrosFavoritos: [
-    {
-      titulo: "O Pior Dia de Todos",
-      autor: "Daniela Kopsch",
-      editora: "Tordesilhas",
+let order = {
+  name: "Rafael Andrade",
+  phoneNumber: "11-98763-1416",
+  address: {
+    street: "Rua das Flores",
+    number: "389",
+    apartment: "701",
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      },
     },
-  ],
+    drinks: {
+      coke: {
+        type: "Coca-Cola Zero",
+        price: 10,
+        amount: 1,
+      },
+    },
+    delivery: {
+      deliveryPerson: "Ana Silveira",
+      price: 5,
+    },
+  },
+  payment: {
+    total: 60,
+  },
 };
 
-console.log(
-  "O livro favorito de " +
-    leitor.nome +
-    " " +
-    leitor.sobrenome +
-    ' se chama "' +
-    leitor.livrosFavoritos[0].titulo +
-    '"'
-);
+function customerInfo(order) {
+  let deliveryPerson = order.order.delivery.deliveryPerson;
+  let customer = order.name;
+  let phoneNumber = order.phoneNumber;
+  let street = order.address.street;
+  let number = order.address.number;
+  let apt = order.address.apartment;
 
-leitor.livrosFavoritos.push({
-  titulo: "Harry Potter e o Prisioneiro de Azkaban",
-  autor: "JK Rowling",
-  editora: "Rocco",
-});
+  console.log(
+    "Olá, " +
+      deliveryPerson +
+      ", entrega para: " +
+      customer +
+      ", Telefone: " +
+      phoneNumber +
+      ", " +
+      street +
+      ", Nº: " +
+      number +
+      ", AP: " +
+      apt
+  );
+}
 
-console.log("Julia tem " + leitor.livrosFavoritos.length + " livros favoritos");
+customerInfo(order);
+
+function orderModifier(order) {
+  order.name = "Luiz Silva";
+  order.payment.total = "50,00";
+  let customer = order.name;
+  let pizzas = Object.keys(order.order.pizza);
+  let drinks = order.order.drinks.coke.type;
+  let newTotal = order.payment.total;
+
+  console.log(
+    "Olá " +
+      customer +
+      ", o total do seu pedido de " +
+      pizzas[0] +
+      ", " +
+      pizzas[1] +
+      " é R$ " +
+      newTotal
+  );
+}
+
+orderModifier(order);
