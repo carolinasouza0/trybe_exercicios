19218f174e2a0beb817cdaf90fe066290083af01
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-03-03 18:27:58 -0300

    ternary operator, exemplo

--- Fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ Fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,6 +1,6 @@
-const nomeCompleto = (nome, sobrenome) => {
-  return `${nome} ${sobrenome}`;
-};
-let nome = "Ivan";
-let sobrenome = "Pires";
-console.log(nomeCompleto(nome, sobrenome));
+let speed = 90;
+const speedCar = (speed) =>
+  speed >= 120
+    ? "Você ultrapassou o limite de velocidade"
+    : "Você está na velocidade permitida";
+console.log(speedCar(speed));
