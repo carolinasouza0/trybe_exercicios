9852669ba05a92a2cd4999f29dedb13b5385a51c
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-03-06 10:22:45 -0300

    transformando algarismos romanos em números

--- Fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/script.js
+++ Fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/script.js
@@ -1,46 +1,20 @@
-let lesson1 = {
-  materia: "Matemática",
-  numeroEstudantes: 20,
-  professor: "Maria Clara",
-  turno: "manhã",
+const romanAlgarism = (string) => {
+  const splitAlgarism = string.split("");
+
+  const roman = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  const number = splitAlgarism.map((numeral) => roman[numeral]);
+
+  return number.reduceRight((ac, current, index, array) => {
+    return current < array[index + 1] ? ac - current : ac + current;
+  });
 };
 
-let lesson2 = {
-  materia: "História",
-  numeroEstudantes: 20,
-  professor: "Carlos",
-};
-
-let lesson3 = {
-  materia: "Matemática",
-  numeroEstudantes: 10,
-  professor: "Maria Clara",
-  turno: "noite",
-};
-
-function nightShift(obj, key, value) {
-  obj[key] = value;
-}
-nightShift(lesson2, "turno", "noite");
-console.log(lesson2);
-
-function keyList(obj) {
-  return Object.keys(obj);
-}
-console.log(keyList(lesson1));
-
-function objectSize(obj) {
-  return Object.keys(obj).length;
-}
-console.log(objectSize(lesson1));
-
-function objectValues(obj) {
-  return Object.values(obj);
-}
-console.log(objectValues(lesson1));
-
-let allLessons = Object.assign(
-  {},
-  { lesson1: lesson1, lesson2: lesson2, lesson3: lesson3 }
-);
-console.log(allLessons);
+console.log(romanAlgarism("MMXVIII"));
