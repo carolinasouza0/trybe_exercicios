200cc55450a571cb76990da65104b609d5279f4c
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-03-13 20:26:23 -0300

    manipulando eventos e web storage no browser

--- Fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
+++ Fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
@@ -1,18 +1,42 @@
-const myObj = {
-  name: "Link",
-  age: 20,
+const body = document.querySelector("body");
+const backgroundWhite = document.querySelector("#white");
+const backgroundBlack = document.querySelector("#black");
+const backgroundGreen = document.querySelector("#green");
+const backgroundBlue = document.querySelector("#blue");
+const backgroundYellow = document.querySelector("#yellow");
+
+backgroundWhite.addEventListener("click", () => {
+  body.style.backgroundColor = "white";
+  localStorage.setItem("backgroundColor", "white");
+});
+
+backgroundBlack.addEventListener("click", () => {
+  body.style.backgroundColor = "black";
+  localStorage.setItem("backgroundColor", "black");
+});
+
+backgroundGreen.addEventListener("click", () => {
+  body.style.backgroundColor = "green";
+  localStorage.setItem("backgroundColor", "green");
+});
+backgroundBlue.addEventListener("click", () => {
+  body.style.backgroundColor = "blue";
+  localStorage.setItem("backgroundColor", "blue");
+});
+backgroundYellow.addEventListener("click", () => {
+  body.style.backgroundColor = "yellow";
+  localStorage.setItem("backgroundColor", "yellow");
+});
+
+const meuLocalStorage = {
+  setItem: (chave, valor) => localStorage.setItem(chave, JSON.stringify(valor)),
+  getItem: (chave) => {
+    const obtem = localStorage.getItem(chave);
+    if (obtem) return JSON.parse(obtem);
+    return null;
+  },
 };
 
-// Precisamos transformar este objeto em uma string. Para isso, utilizamos a função `JSON.stringify()`.
-
-localStorage.setItem("myData", JSON.stringify(myObj));
-console.log(localStorage);
-// Como dito anteriormente, o `localStorage` armazena dados no formato chave e valor, sendo os valores sempre string. Para isso, precisamos passar como primeiro parâmetro do método `setItem()` o nome da chave, passamos `myData` e como segundo parâmetro, a função `JSON.stringify()` com o objeto-valor que queremos armazenar na chave `myData`. Feito isso, teremos salvo no `localStorage` um objeto com a chave `myData` e o valor `{ "name": "Link", "age": 20 }`.
-
-// Agora, precisamos recuperar o objeto armazenado, mas queremos ele no formato original e não como uma string. Para isso, utilizaremos a função `JSON.parse()`.
-
-const recoveredObject = JSON.parse(localStorage.getItem("myData"));
-
-// Utilizamos a função `JSON.parse()` para transformar a string que está armazenada no `localStorage` de volta para seu formato original, um objeto.
-
-console.log(recoveredObject); // { name: 'Link', age: 20 }
+window.onload = () => {
+  body.style.backgroundColor = localStorage.getItem("backgroundColor");
+};
