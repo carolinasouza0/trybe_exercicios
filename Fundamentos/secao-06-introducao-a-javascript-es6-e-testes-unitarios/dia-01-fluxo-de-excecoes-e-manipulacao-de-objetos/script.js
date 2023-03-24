356a06dbb0c9382847b7c02deda956e32f9336ae
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-03-24 15:34:09 -0300

    entendendo fluxo de esceção com throw e try/catch

--- Fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-01-fluxo-de-excecoes-e-manipulacao-de-objetos/script.js
+++ Fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-01-fluxo-de-excecoes-e-manipulacao-de-objetos/script.js
@@ -1,18 +1,70 @@
-const verifyIsNumber = (value1, value2) => {
-  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
-    throw new Error('Os valores devem ser numéricos'); // através do construtor `new Error` já sabemos que será lançada a mensagem se algo der errado.
-  }
-};
+// Primeira parte
+const promo = [
+  {product: 'bola de beach tennis', price: 29.99},
+  {product: 'mochila de trilha 20 litros', price: 120.99},
+  {product: 'capacete de ciclista', price: 180.59},
+  {product: 'toalha compacta de secagem', price: 39.99},
+  {product: 'kit 5 camisetas Dry Fit', price: 99.99},
+  {product: 'caneleiras de peso - 3kg (par)', price: 44.99},
+  {product: 'bola de ginástica e pilates', price: 149.59},
+  {product: 'óculos de natação', price: 18.99},
+  {product: 'capa protetora de prancha de surf', price: 89.59},
+  {product: 'bola de basquete', price: 39.99},
+];
 
-const sum = (value1, value2) => {
-  // o `try` vai executar a função verifyNumber e retornar a soma, caso os parâmetros sejam números
-  try { 
-    verifyIsNumber(value1, value2);
-    return value1 + value2;
-  // se o bloco `try` apresentar erro, o `catch` vai capturar esse erro e retornar a mensagem já criada por meio da propriedade `.message` nativa do objeto Error.
-  } catch (error) {
-    return error.message;
-  }
-};
+const button = document.querySelector('#send-button');
 
-console.log(sum(2, '3'));
+button.addEventListener('click', () => {
+  const name = document.querySelector('#name-id').value;
+  const number = document.querySelector('#number-id').value;
+
+  showPromo(name, number);
+});
+
+const showPromo = (name, number) => {
+  const firstText = document.querySelector('#text-initial');
+  const secondText = document.querySelector('#text-final');
+  
+  try {
+    checkName(name);
+    checkNumber(parseInt(number));
+    checkValidRange(number);
+    const productObject = checkPromo(number);
+
+    firstText.innerHTML = `Boas-vindas, ${name}!`;
+    secondText.innerHTML = `A promoção do dia é: 
+      ${productObject.product} no valor de R$ ${productObject.price}`;
+  } catch(err) {
+    secondText.innerHTML = err.message;
+  }
+}
+
+const checkName = (name) => {
+  let letters = /[aA-zZ]+/;
+  
+  if (!name.match(letters)) {
+    throw new Error('É necessário digitar um nome válido');
+  }
+}
+
+// Segunda parte
+const checkNumber = (number) => {
+  if (isNaN(number)) {
+    throw new Error('É necessário digitar um número');
+  }
+}
+
+// Terceira parte
+const checkPromo = (number) => {
+  for (let index = 0; index < promo.length; index += 1) {
+    if (number - 1 === index) {
+      return promo[index];
+    }
+  }
+}
+
+const checkValidRange = (number) => {
+  if (number < 1 || number > 10) {
+    throw new Error('É necessário digitar um número entre 1 e 10');
+  }
+}
