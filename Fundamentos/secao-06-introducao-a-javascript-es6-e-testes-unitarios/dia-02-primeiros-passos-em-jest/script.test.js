921f90d7e8d8466c9f2ac9d97806a2a6c4c15e92
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-03-27 19:28:57 -0300

    criando função techList baseado nos testes unitários descritos

--- Fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/script.test.js
+++ Fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/script.test.js
@@ -1,34 +1,38 @@
-const { encode, decode } = require('./script');
+const techList = require('./techList.js');
 
-describe ('Testa as funções encode e decode', () => {
-  it('Testa se encode e decode são funções', () => {
-    expect(typeof encode).toBe('function');
-    expect(typeof decode).toBe('function');
+describe('Testa a função techList', () => {
+  it('Testa se a função techList é definida', () => {
+    expect(techList).toBeDefined();
   });
-
-  it('Testa a função encode, se as vogais a, e, i, o, u, são convertidos nos números 1, 2, 3, 4 e 5, respectivamente', () => {
-    expect(encode('ana')).toBe('1n1');
-    expect(encode('ele')).toBe('2l2');
-    expect(encode('tripa')).toBe('tr3p1');
-    expect(encode('ovo')).toBe('4v4');
-    expect(encode('mudo')).toBe('m5d4');
+  it('Testa se techList é uma função', () => {
+    expect(typeof techList).toBe('function');
   });
-
-  it('Testa a função decode, se os números 1, 2, 3, 4 e 5 são convertidos nas vogais a, e, i, o, u, respectivamente', () => {
-    expect(decode('1n1')).toBe('ana');
-    expect(decode('2l2')).toBe('ele');
-    expect(decode('tr3p1')).toBe('tripa');
-    expect(decode('4v4')).toBe('ovo');
-    expect(decode('m5d4')).toBe('mudo');
+  it('Lista com 5 tecnologias deve retornar uma lista de objetos ordenados', () => {
+    expect(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas')).toEqual([
+      {
+        tech: 'CSS',
+        name: 'Lucas'
+      },
+      {
+        tech: 'HTML',
+        name: 'Lucas'
+      },
+      {
+        tech: 'JavaScript',
+        name: 'Lucas'
+      },
+      {
+        tech: 'Jest',
+        name: 'Lucas'
+      },
+      {
+        tech: 'React',
+        name: 'Lucas'
+      }
+    ]);
   });
-
-  it('Testa se as demais letras/números não são convertidos para cada caso', () => {
-    expect(encode('n')).toBe('n');
-    expect(decode('8')).toBe('8');
+  it('Lista com 0 tecnologias deve retornar uma mensagem de erro "Vazio!"', () => {
+    expect(techList([], 'Lucas')).toBe('Vazio!');
   });
-
-  it('Teste se a string que é retornada pelas funções tem o mesmo número de caracteres que a string passada como parâmetro.', () => {
-    expect(encode('carol').length).toEqual(5);
-    expect(decode('carol').length).toEqual(5);
-  })
 });
+
