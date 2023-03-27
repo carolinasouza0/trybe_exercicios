a9c7bad5bfe351f6fba0441fe7397a80b05cec8f
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-03-27 19:04:54 -0300

    fazendo testes unitários com jest para função encode e decode

--- Fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/script.js
+++ Fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/script.js
@@ -1,11 +1,39 @@
-function myRemove(arr, item) {
-  let newArr = [];
-  for (let index = 0; index < arr.length; index += 1) {
-    if (item !== arr[index]) {
-      newArr.push(arr[index]);
+const mapString = (objectMap, string) => {
+  const splitString = string.split('');
+  const mappedArray = [];
+  for (let index = 0; index < splitString.length; index += 1) {
+    const character = splitString[index];
+    const mappedValue = objectMap[character];
+    if (mappedValue) {
+      mappedArray.push(mappedValue);
+    } else {
+      mappedArray.push(character);
     }
   }
-  return newArr;
-}
+  return mappedArray.join('');
+};
 
-module.exports = myRemove;
+const encode = (string) => {
+  const map = {
+    a: 1,
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5,
+  };
+  return mapString(map, string);
+};
+
+const decode = (string) => {
+  const map = {
+    1: 'a',
+    2: 'e',
+    3: 'i',
+    4: 'o',
+    5: 'u',
+  };
+  return mapString(map, string);
+};
+
+const functions = { encode, decode };
+module.exports = functions;
--- Fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/script.test.js
+++ Fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/script.test.js
@@ -1,17 +1,34 @@
-const myRemove = require('./script');
+const { encode, decode } = require('./script');
 
-describe ('testa a função myRemove', () => {
-  const array = [1, 2, 3, 4]
-
-  it ('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
-    expect(myRemove(array, 3)).toEqual([1, 2, 4]);
+describe ('Testa as funções encode e decode', () => {
+  it('Testa se encode e decode são funções', () => {
+    expect(typeof encode).toBe('function');
+    expect(typeof decode).toBe('function');
   });
 
-  it ('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
-    expect(myRemove(array, 3)).not.toEqual([1, 2, 3, 4]);
+  it('Testa a função encode, se as vogais a, e, i, o, u, são convertidos nos números 1, 2, 3, 4 e 5, respectivamente', () => {
+    expect(encode('ana')).toBe('1n1');
+    expect(encode('ele')).toBe('2l2');
+    expect(encode('tripa')).toBe('tr3p1');
+    expect(encode('ovo')).toBe('4v4');
+    expect(encode('mudo')).toBe('m5d4');
   });
 
-  it ('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado.', () => {
-    expect(myRemove(array, 5)).toEqual([1, 2, 3, 4]);
+  it('Testa a função decode, se os números 1, 2, 3, 4 e 5 são convertidos nas vogais a, e, i, o, u, respectivamente', () => {
+    expect(decode('1n1')).toBe('ana');
+    expect(decode('2l2')).toBe('ele');
+    expect(decode('tr3p1')).toBe('tripa');
+    expect(decode('4v4')).toBe('ovo');
+    expect(decode('m5d4')).toBe('mudo');
   });
-})
+
+  it('Testa se as demais letras/números não são convertidos para cada caso', () => {
+    expect(encode('n')).toBe('n');
+    expect(decode('8')).toBe('8');
+  });
+
+  it('Teste se a string que é retornada pelas funções tem o mesmo número de caracteres que a string passada como parâmetro.', () => {
+    expect(encode('carol').length).toEqual(5);
+    expect(decode('carol').length).toEqual(5);
+  })
+});
