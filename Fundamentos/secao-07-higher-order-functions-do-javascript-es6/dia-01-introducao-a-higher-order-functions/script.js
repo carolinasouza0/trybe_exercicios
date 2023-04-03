0334bba5b799bd6509ba1847087ddf648c3ab915
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-04-03 19:20:37 -0300

    utilizando a HoF find()

--- Fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/script.js
+++ Fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/script.js
@@ -1,23 +1,69 @@
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
 
-const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
-const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+const findAuthor = (books) => {
+  let objAuthor = books.find((book) => book.author.birthYear === 1947);
+  return objAuthor.author.name;
+};
 
-const corrector = (rightAnswers, studentAnswers, callback) => {
-  let points = 0;
-  for (let index in rightAnswers) {
-    const returned = callback(rightAnswers[index], studentAnswers[index]);
-    points += returned;
-  }
-  return `Resultado final: ${points} pontos`;
-}
-
-const checkAnswers = (rightAnswer, answer) => {
-    if (answer === rightAnswer) {
-      return 1;
-    } else if (answer === 'N.A') {
-      return 0;
-    }
-    return -0.5;
-}
-
-console.log(corrector(RIGHT_ANSWERS, STUDENT_ANSWERS, checkAnswers));
+console.log(findAuthor(books));
