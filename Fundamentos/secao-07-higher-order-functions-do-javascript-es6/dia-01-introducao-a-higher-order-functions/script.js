ed795306b6b91de2c5d3b5ed4ad9f6963649b1e7
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-04-03 19:01:29 -0300

    criando HOFS 2

--- Fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/script.js
+++ Fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/script.js
@@ -1,11 +1,23 @@
-const lotery = (number, callback) => {
-  let randomNumber = Math.floor(Math.random() * 5);
-  return callback(number, randomNumber) ? 'Parabéns, você ganhou' : 'Tente novamente';
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const corrector = (rightAnswers, studentAnswers, callback) => {
+  let points = 0;
+  for (let index in rightAnswers) {
+    const returned = callback(rightAnswers[index], studentAnswers[index]);
+    points += returned;
+  }
+  return `Resultado final: ${points} pontos`;
 }
 
+const checkAnswers = (rightAnswer, answer) => {
+    if (answer === rightAnswer) {
+      return 1;
+    } else if (answer === 'N.A') {
+      return 0;
+    }
+    return -0.5;
+}
 
-const checkNumber = (number, random) => {
-  return number === random ? true : false;
-  }
-
-  console.log(lotery(2,checkNumber));
+console.log(corrector(RIGHT_ANSWERS, STUDENT_ANSWERS, checkAnswers));
