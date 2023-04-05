145976d4bf6ec2cee952219e2f1310473bcff9fc
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-04-05 20:17:51 -0300

    calculando media de alunos e retornando array de objetos com nomes e médias

--- Fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/script.js
+++ Fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/script.js
@@ -1,7 +1,23 @@
-const names = [
-  'Aanemarie', 'Adervandes', 'Akifusa',
-  'Abegildo', 'Adicellia', 'Aladonata',
-  'Abeladerco', 'Adieidy', 'Alarucha',
+// O index 0 do array `students` equivale ao index 0 do array `grades`
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+const expectedResult = [
+ { name: 'Pedro Henrique', average: 7.8 },
+ { name: 'Miguel', average: 9.2 },
+ { name: 'Maria Clara', average: 8.8 },
 ];
-const splitingArray = names.join('').toLocaleLowerCase().split('');
-const countA = () => splitingArray.reduce((acc, letter) => letter === 'a' ? acc += 1 : acc, 0);
+
+// const averageStudents = () => grades.reduce((acc, grade, index) => {
+//   const sum = acc + grade[index];
+//   const media = sum / grades[index].length;
+//   return media;
+// }, 0);
+
+const studentAverage = () => {
+  return students.map((student, index) => (
+    {
+      name: student,
+      average: grades[index].reduce((acc, grade) => acc + grade, 0) / grades[index].length,
+    }
+  ));
+};
