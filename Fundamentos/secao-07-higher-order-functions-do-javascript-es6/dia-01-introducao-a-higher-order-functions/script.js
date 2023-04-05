38211989cc68c391decd2449a4f51749bcdd0e24
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-04-05 20:02:53 -0300

    encontrando letra específica em array e somando aparições nas strings com reduce

--- Fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/script.js
+++ Fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/script.js
@@ -1,68 +1,61 @@
-const books = [
-  {
-    id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
-    author: {
-      name: 'George R. R. Martin',
-      birthYear: 1948,
-    },
-    releaseYear: 1991,
-  },
-  {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: {
-      name: 'J. R. R. Tolkien',
-      birthYear: 1892,
-    },
-    releaseYear: 1954,
-  },
-  {
-    id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
-    author: {
-      name: 'Isaac Asimov',
-      birthYear: 1920,
-    },
-    releaseYear: 1951,
-  },
-  {
-    id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
-    author: {
-      name: 'Frank Herbert',
-      birthYear: 1920,
-    },
-    releaseYear: 1965,
-  },
-  {
-    id: 5,
-    name: 'A Coisa',
-    genre: 'Terror',
-    author: {
-      name: 'Stephen King',
-      birthYear: 1947,
-    },
-    releaseYear: 1986,
-  },
-  {
-    id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
-    author: {
-      name: 'H. P. Lovecraft',
-      birthYear: 1890,
-    },
-    releaseYear: 1928,
-  },
-];
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
 
-const authorUnique = () => {
-  return books.every((book) => !books.some((bookSome) => (bookSome.author.birthYear === book.author.birthYear) && (bookSome.author.name !== book.author.name)));
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
+
+const dragonDamage = () => {
+  const min = 15;
+  const max = Math.floor(Math.random() * dragon.strength);
+  const dragonDmg = max > min ? max : min;
+  return dragonDmg;
+};
+
+const warriorDamage = () => {
+  const min = warrior.strength;
+  const max = Math.floor(Math.random() * (min * warrior.weaponDmg));
+  const warriorDmg = max > min ? max : min;
+  return warriorDmg;
 }
 
-console.log(authorUnique());
+const mageDamage = () => {
+  const mana = mage.mana;
+  const min = mage.intelligence;
+  const max = min * 2;
+  const objMage = {
+    manaSpent: 0,
+    damageDealt: "Não possui mana suficiente",
+  };
+
+  if (mana > 15) {
+    const mageDmg = min < max ? max : min;
+    objMage.manaSpent = 15;
+    objMage.damageDealt = mageDmg
+    return objMage;
+  }
+  return objMage;
+}
+
+const warriorTurn = () => {
+  
+}
+
+const gameActions = {
+  // Crie as HOFs neste objeto.
+};
