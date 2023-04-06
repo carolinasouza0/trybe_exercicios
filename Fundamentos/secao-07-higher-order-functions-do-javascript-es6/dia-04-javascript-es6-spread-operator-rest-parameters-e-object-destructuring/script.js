6667306a66af75709b6764975e44c1b8f93e6863
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-04-06 15:38:00 -0300

    utilizando rest parameters para calcular soma de infinita quantidade de números

--- Fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-spread-operator-rest-parameters-e-object-destructuring/script.js
+++ Fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-spread-operator-rest-parameters-e-object-destructuring/script.js
@@ -1,11 +1,2 @@
-const rectangleArea = (width, height) => width * height;
-
-const rectangle1 = [1, 2];
-const rectangle2 = [3, 5];
-const rectangle3 = [6, 9];
-const rectangles = [rectangle1, rectangle2, rectangle3];
-
-
-rectangles.forEach((rectangle) => {
-  console.log(rectangleArea(...rectangle)); // Altere o parâmetro recebido por rectangleArea()
-});
+const sum = (...args) => args.reduce((acc, arg) => acc + arg);
+console.log(sum(4,5,6, 10, 25));
