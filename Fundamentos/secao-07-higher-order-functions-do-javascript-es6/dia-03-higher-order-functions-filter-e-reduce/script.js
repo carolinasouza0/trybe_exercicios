b35fc2ec40bcecee1c059c6550401dca19f0dd7d
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-04-05 21:42:42 -0300

    utilizando reduce() e concat() para concatenar array de arrays

--- Fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/script.js
+++ Fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/script.js
@@ -1,23 +1,9 @@
-// O index 0 do array `students` equivale ao index 0 do array `grades`
-const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
-const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
-const expectedResult = [
- { name: 'Pedro Henrique', average: 7.8 },
- { name: 'Miguel', average: 9.2 },
- { name: 'Maria Clara', average: 8.8 },
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
 ];
 
-// const averageStudents = () => grades.reduce((acc, grade, index) => {
-//   const sum = acc + grade[index];
-//   const media = sum / grades[index].length;
-//   return media;
-// }, 0);
+const flatten = () => arrays.reduce((acc, value) => acc.concat(value));
 
-const studentAverage = () => {
-  return students.map((student, index) => (
-    {
-      name: student,
-      average: grades[index].reduce((acc, grade) => acc + grade, 0) / grades[index].length,
-    }
-  ));
-};
+console.log(flatten());
